66a651a63298e1e7b49453af3680b88b125c9d49
Allan Souza <allanronaldo95@gmail.com>
2021-06-15 14:09:50 -0300

    Criando exibição e filtro dos produtos

--- js/main.js
+++ js/main.js
@@ -1,62 +1,91 @@
 const produtos = [
   {
     id: 0,
-    nome: 'Tenis 1',
-    cor: 'preto',
-    img: './img/imagemCarrinho.png',
+    nome: 'Tênis',
+    cor: 'Preto',
+    img: './img/tenis-1.png',
     valor: '149.99'
   },
   {
     id: 1,
-    nome: 'Tenis 2',
-    cor: 'rosa',
-    img: './img/imagemCarrinho.png',
+    nome: 'Tênis',
+    cor: 'Cinza e Laranja',
+    img: './img/tenis-2.png',
     valor: '159.99'
   }, 
   {
     id: 2,
-    nome: 'Tenis 3',
-    cor: 'bege',
-    img: './img/imagemCarrinho.png',
+    nome: 'Tênis',
+    cor: 'Azul e Preto',
+    img: './img/tenis-3.png',
     valor: '249.99'
   },
   {
     id: 3,
-    nome: 'Tenis 4',
-    cor: 'verde',
-    img: './img/imagemCarrinho.png',
+    nome: 'Sapato',
+    cor: 'Preto',
+    img: './img/tenis-4.png',
     valor: '129.99'
   },
   {
     id: 4,
-    nome: 'Tenis 5',
-    cor: 'vermelho',
-    img: './img/imagemCarrinho.png',
+    nome: 'All-Star',
+    cor: 'Vermelho',
+    img: './img/tenis-5.png',
     valor: '139.99'
   }, 
   {
     id: 5,
-    nome: 'Tenis 6',
-    cor: 'lilas',
-    img: './img/imagemCarrinho.png',
+    nome: 'Tênis',
+    cor: 'Azul',
+    img: './img/tenis-6.png',
     valor: '219.99'
   }
 ];
 
 exibirProdutos = () => {
   var listaProdutos = document.getElementById('produtos');
-  let img = document.createElement("img");
-  produtos.map((valor) => {
-    listaProdutos.innerHTML += `
-      <div class="col-md-4 mb-5">
-        <img src="`+valor.img+`">
-        <p>`+valor.nome+ " - " + valor.cor+`</p>
-        <p>`+"R$ " +valor.valor+`</p>
-        <a key=`+valor.id+` href="#">Adicionar ao carrinho</a>
-      </div>
-      `
-    console.log(valor.nome);
-  })
+  var filtro = document.getElementById('filtroProdutos');
+  listaProdutos.innerHTML = '';
+
+  if (filtro.value == 'Todos') {
+    produtos.map((valor) => {
+      listaProdutos.innerHTML += `
+        <div class="col-md-4 mb-5">
+          <img src="`+valor.img+`" class="img-fluid">
+          <p>`+valor.nome+ " - " + valor.cor+`</p>
+          <p>`+"R$ " +valor.valor+`</p>
+          <a key=`+valor.id+` href="#">Adicionar ao carrinho</a>
+        </div>
+        `
+    }
+    )} else if (filtro.value == 'Tenis') {
+      produtos.map((valor) => {
+        if (valor.nome === 'Tênis') {
+          listaProdutos.innerHTML += `
+            <div class="col-md-4 mb-5">
+              <img src="`+valor.img+`" class="img-fluid">
+              <p>`+valor.nome+ " - " + valor.cor+`</p>
+              <p>`+"R$ " +valor.valor+`</p>
+              <a key=`+valor.id+` href="#">Adicionar ao carrinho</a>
+            </div>
+            `;
+        }
+      }
+      )} else if (filtro.value == 'Sapatos') {
+        produtos.map((valor) => {
+          if (valor.nome != 'Tênis') {
+            listaProdutos.innerHTML += `
+              <div class="col-md-4 mb-5">
+                <img src="`+valor.img+`" class="img-fluid">
+                <p>`+valor.nome+ " - " + valor.cor+`</p>
+                <p>`+"R$ " +valor.valor+`</p>
+                <a key=`+valor.id+` href="#">Adicionar ao carrinho</a>
+              </div>
+              `;
+           }
+          }
+        )}
 };
 
 exibirProdutos();
